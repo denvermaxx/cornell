8c575f1ebb41f896cf9426be09b58de5432c4213
Shelby White <shelby.white@realart.com>
2017-04-06 23:50:22 -0400

    product page init

---
+++ app/product.php
@@ -0,0 +1,106 @@
+
+
+-
+<?php
+include 'inc/head.php';
+include 'inc/nav.php';
+?>
+
+    <!-- Primary Page Layout -->
+
+
+<section id="product_details" class="flex flex--between">
+    <div class="col col--12 col__md--4">
+      <span class="product_title">
+        WC899 Cone 10 Woodfire B Mix
+      </span>
+      <span class="product_price">
+        $28.50
+      </span>
+      <span class="stock">
+        In Stock
+      </span>
+      <p class="product_desc">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+        nostrud exercitation ullamco laboris nisi ut aliquip ex ea consequat.
+      </p>
+      <select>Shipping Method</select>
+      <table class="shipping_price_qty" cellpadding="0" style="border:1px #742b02 solid;">
+      <tr bgcolor="#742b02" style="color:#fff; text-align:center;">
+        <td>Price</td>
+        <td>QTY</td>
+      </tr>
+      <tr>
+        <td>$26.50</td>
+        <td>1 Box / 50lbs</td>
+      </tr>
+      </table>
+
+    </div>
+    <div class="col--12 col__md--8">
+    </div>
+</section>
+
+
+    <section id="home_products" class="flex flex--between">
+        <div class="col col--12">
+            <h2>Lorem Ipsum Dolor</h2>
+        </div>
+        <div class="col col--12 col__md--6 col__lg--3">
+            <div class="item">
+                <a href="">
+                    <img src="img/product.png">
+                    <span class="product_name">Product Name</span>
+                    <span class="price">$5.99</span>
+                </a>
+            </div>
+        </div>
+        <div class="col col--12 col__md--6 col__lg--3">
+            <div class="item">
+                <a href="">
+                    <img src="img/product.png">
+                    <span class="product_name">Product Name</span>
+                    <span class="price">$5.99</span>
+                </a>
+            </div>
+        </div>
+        <div class="col col--12 col__md--6 col__lg--3">
+            <div class="item">
+                <a href="">
+                    <img src="img/product.png">
+                    <span class="product_name">Product Name</span>
+                    <span class="price">$5.99</span>
+                </a>
+            </div>
+        </div>
+        <div class="col col--12 col__md--6 col__lg--3">
+            <div class="item">
+                <a href="">
+                    <img src="img/product.png">
+                    <span class="product_name">Product Name</span>
+                    <span class="price">$5.99</span>
+                </a>
+            </div>
+        </div>
+    </section>
+
+    <section id="home_classes" class="flex flex--auto">
+        <div class="col col--12 col__md--4">
+            <div class="feature-side red">
+                <div class="text">
+                    <span>Classes, Workshops, &amp; Demonstrations at Cornell Studio Supply</span>
+                    <p>Brought to you by amazingly talented and awesomely fun instructors!</p>
+                    <a href="" class="button">see class schedule</a>
+                </div>
+            </div>
+        </div>
+        <div class="col col--12 col__md--8 hide_mobile">
+            <div class="featured"></div>
+        </div>
+    </section>
+
+    <?php
+    include 'inc/footer.php';
+    include 'inc/end.php';
+    ?>
